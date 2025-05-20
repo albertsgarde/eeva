d8d71395a1de0ae792d12e89ad26044ef88bc176
Albert Garde <albertsgarde@gmail.com>
2025-05-20 11:24:31 +0200

    :lipstick: Make frontend a little more mobile friendly

--- interview-frontend/src/routes/+page.svelte
+++ interview-frontend/src/routes/+page.svelte
@@ -65,9 +65,9 @@
 </script>
 
 <!-- Body -->
-<div class="flex h-screen items-center justify-center bg-gray-100 p-4">
+<div class="flex h-dvh items-center justify-center bg-gray-100 p-4">
 	<!-- Main Chat Container -->
-	<div class="h-256 flex w-full max-w-screen-xl flex-col rounded-lg bg-white shadow-md">
+	<div class="w-256 flex h-[95vh] max-h-[95vh] max-w-[95vw] flex-col rounded-lg bg-white shadow-md">
 		<!-- Chat Header -->
 		<div class="rounded-t-lg bg-blue-600 p-4 text-white">
 			<h1 class="text-xl font-bold">Eeva</h1>
