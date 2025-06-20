f3265fb726d457cb7a9de27f5e6a9bb4701762b2
Albert Garde <albertsgarde@gmail.com>
2025-06-20 14:44:18 +0200

    :fire: Remove name input from site

--- interview-frontend/src/routes/form-responses/[formResponseId]/+page.svelte
+++ interview-frontend/src/routes/form-responses/[formResponseId]/+page.svelte
@@ -46,36 +46,13 @@
 	}
 </script>
 
-<div class="flex">
-	<!-- Name input area -->
-	<div class="w-1/3 p-4">
-		<label for="userName" class="mb-2 block text-gray-200">Your Name</label>
-		<input
-			id="userName"
-			type="text"
-			bind:value={subjectName}
-			placeholder="Enter your name"
-			class="mb-2 mr-1 max-w-[60ch] rounded border border-gray-600 bg-gray-700 p-2 text-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
-		/>
-		<button
-			on:click={saveSubjectName}
-			class="rounded bg-green-600 px-4 py-2 font-bold text-white hover:bg-green-700 focus:outline-none active:bg-green-800 disabled:cursor-not-allowed disabled:bg-gray-600 disabled:opacity-50"
-			disabled={subjectName === formResponse.subjectName}
-		>
-			Save Name
-		</button>
-	</div>
-
-	<!-- Form questions scrollable div -->
-	<div class="mx-auto w-1/3 max-w-[80ch] overflow-y-auto p-1">
+<div class="mx-auto flex max-w-2xl flex-col">
+	<div class="overflow-y-auto p-1">
 		{#each formResponse.responses as questionResponse, index}
 			<FormQuestion
 				{questionResponse}
 				onSave={(response: string) => saveResponse(response, index)}
 			/>
 		{/each}
-	</div>
-	<div class="w-1/3 p-4">
-		<!-- Placeholder for additional content or actions -->
 	</div>
 </div>
