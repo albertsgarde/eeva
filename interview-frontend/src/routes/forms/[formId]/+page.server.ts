e19877e94a8d9dff85d027ca70c5029efb98d759
Albert Garde <albertsgarde@gmail.com>
2025-06-24 21:16:42 +0200

    :goal_net: Give error when form doesn't exist

--- interview-frontend/src/routes/forms/[formId]/+page.server.ts
+++ interview-frontend/src/routes/forms/[formId]/+page.server.ts
@@ -1,11 +1,17 @@
 import { expect, PromptId, type CreateInterviewRequest, type FormResponseId, type InterviewId, type Message, type FormId } from "$lib/base";
+import { error } from "@sveltejs/kit";
 
 export interface Data {
     formId: FormId;
 }
 
 export async function load({ fetch, params }: { fetch: any, params: {formId: FormId} }): Promise<Data> {
-    const {formId} = params;    
+    const {formId} = params;
+
+    const response = await fetch(`/api/forms/${formId}`);
+    if (!response.ok) {
+        error(404, 'Form not found');
+    }
 
     return { formId };
 }
