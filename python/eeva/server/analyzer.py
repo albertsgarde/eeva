6cf225683109b579515bb5972109dc793d672ace
Albert Garde <albertsgarde@gmail.com>
2025-09-11 11:20:13 +0200

    :poop: Replace calls to OpenAI with stubs

--- python/eeva/server/analyzer.py
+++ python/eeva/server/analyzer.py
@@ -14,12 +14,16 @@ def create_router(database: Database, llm: BaseChatModel, data_path: Path) -> AP
 
     @router.post("/analyze")
     async def analyze(response: Response) -> Profile:
+        print(f"Analyzing response for user {response.first_name}")
+        return Profile(identity=0.0, horoscope="DEBUG")
         return await analyzer.analyze(response, llm, data_path)
 
     @router.post("/analyze-relationship")
     async def analyze_relationship(
         response1: Response, profile1: Profile, response2: Response, profile2: Profile
     ) -> RelationshipProfile:
+        print(f"Analyzing link for users {response1.first_name} and {response2.first_name}")
+        return RelationshipProfile(horoscope="DEBUG")
         return await analyzer.analyze_relationship(response1, profile1, response2, profile2, llm, data_path)
 
     return router
