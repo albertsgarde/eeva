de5ad370e478119aaa16748259ce29409c2e9692
Albert Garde <albertsgarde@gmail.com>
2025-10-20 16:02:53 +0200

    :recycle: Only create messages list once per user

--- python/eeva/experiment/analysis.py
+++ python/eeva/experiment/analysis.py
@@ -1,6 +1,6 @@
 import asyncio
 
-from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_core.messages import BaseMessage, HumanMessage, SystemMessage
 from pydantic import BaseModel, Field, RootModel
 
 from ..models import Model, UsageData
@@ -52,7 +52,7 @@ class Analyzer(BaseModel):
     system_prompt: str | None = Field()
     llm: Model = Field()
 
-    async def analyze(self, content: str) -> AnalysisResult:
+    async def analyze(self, messages: list[BaseMessage]) -> AnalysisResult:
         class CotAnalyzerOutput(BaseModel):
             """ """
 
@@ -65,14 +65,6 @@ class Analyzer(BaseModel):
             identity: float = Field(ge=0, le=1, description=self.identity_prompt)
 
         output_type = CotAnalyzerOutput if self.explicit_cot else AnalyzerOutput
-
-        if self.system_prompt:
-            messages = [
-                SystemMessage(content=self.system_prompt),
-                HumanMessage(content=content),
-            ]
-        else:
-            messages = [HumanMessage(content=content)]
 
         raw_output, usage_data = await self.llm.get_structured_output(messages, output_type)
 
@@ -89,22 +81,30 @@ class Analyzer(BaseModel):
 
         return AnalysisResult(profile=profile, cot=cot, response_metadata=usage_data)
 
-
-async def generate_user_profiles(
-    analyzer: Analyzer, user_id: UserId, user: User, num_profiles: int
-) -> tuple[UserId, AnalysisResultUser]:
-    tasks = []
-    content = "\n".join(f"{question.question}: {question.response}" for question in user.response.responses.values())
-    for _ in range(num_profiles):
-        tasks.append(asyncio.create_task(analyzer.analyze(content)))
-    profiles = await asyncio.gather(*tasks)
-    result_user = AnalysisResultUser(
-        first_name=user.response.first_name,
-        last_name=user.response.last_name,
-        llm_input=content,
-        analysis_results=profiles,
-    )
-    return (user_id, result_user)
+    async def generate_user_profiles(
+        self, user_id: UserId, user: User, num_profiles: int
+    ) -> tuple[UserId, AnalysisResultUser]:
+        tasks = []
+        content = "\n".join(
+            f"{question.question}: {question.response}" for question in user.response.responses.values()
+        )
+        if self.system_prompt:
+            messages = [
+                SystemMessage(content=self.system_prompt),
+                HumanMessage(content=content),
+            ]
+        else:
+            messages = [HumanMessage(content=content)]
+        for _ in range(num_profiles):
+            tasks.append(asyncio.create_task(self.analyze(messages)))
+        profiles = await asyncio.gather(*tasks)
+        result_user = AnalysisResultUser(
+            first_name=user.response.first_name,
+            last_name=user.response.last_name,
+            llm_input=content,
+            analysis_results=profiles,
+        )
+        return (user_id, result_user)
 
 
 # Create a dict user_id -> Profile for all users in user_data using their responses to run `analyze`
@@ -118,7 +118,7 @@ async def generate_profiles(
     async def analyze_all_users() -> AnalysisResultSet:
         tasks = []
         for user_id, user in user_data.items():
-            tasks.append(asyncio.create_task(generate_user_profiles(analyzer, user_id, user, num_profiles)))
+            tasks.append(asyncio.create_task(analyzer.generate_user_profiles(user_id, user, num_profiles)))
         results: list[tuple[UserId, AnalysisResultUser]] = await asyncio.gather(*tasks)
         return AnalysisResultSet({user_id: result for user_id, result in results})
 
